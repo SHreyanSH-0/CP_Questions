290022a5a647cf95ee098ed681d35d55f453d88c
Shreyansh Tiwari <77192889+SHreyanSH-0@users.noreply.github.com>
2026-09-03 12:08:01 +0530

    Time: 0 ms (100%), Space: 9.2 MB (37.39%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
+    int dp[21][21];
     bool f(string &s,string &p,int i,int j,int &n,int &m){
         if(i==n && j==m)return true;
         if(j==m)return false;
+        if(dp[i][j]!=-1)return dp[i][j];
         if(j+1<m && p[j+1]=='*'){
             for(int k=i;k<=n;k++){
                 if(f(s,p,k,j+2,n,m)){
-                    return true;
+                    return dp[i][j]=true;
                 }
                 if(s[k]==p[j] || p[j]=='.')continue;
                 break;  
             }
-            return false;
+            return dp[i][j]=false;
         }
         else if(i==n){
-            return false;
+            return dp[i][j]=false;
         }
         else if(p[j]=='.'){
-            return f(s,p,i+1,j+1,n,m);
+            return dp[i][j]=f(s,p,i+1,j+1,n,m);
         }
         else if(s[i]==p[j]){
-            return f(s,p,i+1,j+1,n,m);
+            return dp[i][j]=f(s,p,i+1,j+1,n,m);
         }
         else{
-            return false;
+            return dp[i][j]=false;
         }
     }
     bool isMatch(string s, string p) {
+        memset(dp,-1,sizeof(dp));
         int n=s.size();
         int m=p.size();
         return f(s,p,0,0,n,m);
