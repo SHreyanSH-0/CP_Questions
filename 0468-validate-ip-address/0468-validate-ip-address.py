d86493c1319566f5ec9efab8ada94c9acb529db7
Shreyansh Tiwari <77192889+SHreyanSH-0@users.noreply.github.com>
2026-09-04 15:17:18 +0530

    Time: 1 ms (10.29%), Space: 19.5 MB (18.7%) - LeetHub

--- 0468-validate-ip-address/0468-validate-ip-address.py
+++ 0468-validate-ip-address/0468-validate-ip-address.py
@@ -1,47 +1,13 @@
+from ipaddress import ip_address,IPv6Address
 class Solution:
-
-    def isIpv6(self, queryIP):
-        v = queryIP.split('::')
-
-        if len(v) >= 2:
-            return False
-        if(len(v) == 0):
-            v = queryIp
-        n = 0
-        for s in v:
-            hold = s.split(':')
-            n = n + len(hold)
-            for p in hold:
-                if len(p) > 4 or len(p) == 0: return False
-                for c in p:
-                    if not ((c >= 'a' and c <= 'f') or (c >= '0' and c <= '9')or (c >= 'A' and c <= 'F')):
-                        return False
-
-        if n > 8: return False
-        return True
-
-    def isIPv4(self, s):
-        v = s.split('.')
-        if len(v) != 4 : 
-            return False
-        
-        for val in v:
-            if len(val) == 0:
-                return False
-            for c in val:
-                if not (c >= '0' and c <= '9'): return False
-            if int(val) > 255 or int(val) < 0:
-                return False
-            if val[0] == '0' and len(val) > 1:
-                return False
-        
-        return True
-
-
     def validIPAddress(self, queryIP: str) -> str:
-        if self.isIpv6(queryIP) is True:
-            return "IPv6"
-        if self.isIPv4(queryIP) is True:
-            return "IPv4"
-        return "Neither"
+        if queryIP.find('::')!=-1 :
+            return "Neither"
+        try:
+            if type(ip_address(queryIP)) is IPv6Address:
+                return "IPv6"
+            else:
+                return "IPv4"
+        except:
+            return "Neither"
         
