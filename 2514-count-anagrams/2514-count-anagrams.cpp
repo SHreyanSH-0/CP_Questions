36042487e9f361141784e0bc202e658eebd8ac64
Shreyansh Tiwari <77192889+SHreyanSH-0@users.noreply.github.com>
2026-09-17 21:32:26 +0530

    Time: 172 ms (11.4%), Space: 40.3 MB (20.46%) - LeetHub

---
+++ 2514-count-anagrams/2514-count-anagrams.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    long long mod = 1e9 + 7;
+    long long  power(long long a, long long b){
+        long long ans = 1;
+        while( b > 0){
+            if(b%2 == 1) ans = (ans * a)%mod;
+            a = (a*a)%mod;
+            b/=2;
+        }
+        return ans;
+    }
+
+    long long inv(long long n){
+        return power(n,mod - 2);
+    }
+
+    long long fun(string &temp, vector<long long>&infact, vector<long long>&fact){
+        vector<int> freq(26,0);
+
+        for(int i=0;i<temp.size();i++){
+            freq[temp[i] - 'a']++;
+        }
+
+        long long ans = fact[temp.size()];
+        for(int i=0;i<26;i++){
+            ans = (ans * infact[freq[i]])%mod;
+        }
+
+
+        return ans;
+    }
+
+    int countAnagrams(string s) {
+        string temp = "";
+        long long ans = 1;
+
+        vector<long long> infact(s.size() + 1), fact(s.size() + 1);
+
+        infact[0] = fact[0] = 1;
+        for(int i=1;i<=s.size();i++){
+            fact[i] = (fact[i-1]*i)%mod;
+            infact[i] = inv(fact[i]);
+        }
+
+        for(int i=0;i<s.size();i++){
+            if(s[i] == ' ')
+                ans = (ans * fun(temp,infact,fact))%mod , temp = "";
+            else 
+                temp.push_back(s[i]);
+        }
+
+        ans = (ans * fun(temp,infact,fact))%mod;
+
+        return ans;
+    }
+};
